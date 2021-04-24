cb4cafa8eb03db2c61aa10f3fe05556e8cb58be6
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-04-24 13:38:25 -0400

    all models working properly

---
+++ Develop/models/index.js
@@ -0,0 +1,21 @@
+const Location = require('./Location');
+const Traveller = require('./Traveller');
+const Trip = require('./Trip');
+
+Traveller.belongsToMany(Location, {
+  through: {
+    model: Trip,
+    unique: false,
+  },
+  as: "planned_trips",
+});
+
+Location.belongsToMany(Traveller, {
+  through: {
+    model: Trip,
+    unique: false,
+  },
+  as: "location_travellers",
+});
+
+module.exports = { Location, Traveller, Trip };
